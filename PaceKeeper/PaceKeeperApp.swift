1824f980a041708e20a7a3ffbd6b46ec22887a07
ddophi98 <lightcloud98@gmail.com>
2022-05-17 16:35:20 +0900

    Initial Commit

---
+++ PaceKeeper/PaceKeeperApp.swift
@@ -0,0 +1,17 @@
+//
+//  PaceKeeperApp.swift
+//  PaceKeeper
+//
+//  Created by 김동락 on 2022/05/17.
+//
+
+import SwiftUI
+
+@main
+struct PaceKeeperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
