58ce40a54843f884e759d04ab64e9b9fb7fe9c77
ddophi98 <lightcloud98@gmail.com>
2022-05-31 21:45:42 +0900

    그래프 띄우기 성공함

---
+++ PaceKeeper/ChartView.swift
@@ -0,0 +1,81 @@
+//
+//  ChartView.swift
+//  PaceKeeper
+//
+//  Created by 김동락 on 2022/05/31.
+//
+
+import SwiftUI
+import Charts
+
+struct ChartView : UIViewRepresentable {
+    var xVals : [String]
+    var yVals : [ChartDataEntry]
+
+    func makeUIView(context: Context) -> LineChartView {
+        let chart = LineChartView()
+        return createChart(chart: chart)
+    }
+
+    func updateUIView(_ uiView: LineChartView, context: Context) {
+        uiView.data = addData()
+    }
+    
+    // 차트 설정 및 생성
+    func createChart(chart: LineChartView) -> LineChartView{
+        chart.chartDescription?.enabled = false
+        chart.xAxis.drawGridLinesEnabled = false
+        chart.xAxis.drawLabelsEnabled = true
+        chart.xAxis.drawAxisLineEnabled = false
+        chart.xAxis.labelPosition = .bottom
+        chart.rightAxis.enabled = false
+        chart.leftAxis.enabled = false
+        chart.drawBordersEnabled = false
+        chart.legend.form = .none
+        chart.xAxis.labelCount = 7
+        chart.xAxis.forceLabelsEnabled = true
+        chart.xAxis.granularityEnabled = true
+        chart.xAxis.granularity = 1
+        chart.xAxis.valueFormatter = CustomChartFormatter(times: xVals)
+        chart.data = addData()
+        return chart
+    }
+    
+    // 데이터 추가하기
+    func addData() -> LineChartData{
+        let data = LineChartData(dataSets: [
+            generateLineChartDataSet(dataSetEntries: yVals, color: UIColor(Color(#colorLiteral(red: 0.6235294118, green: 0.7333333333, blue: 0.3568627451, alpha: 1))), fillColor: UIColor(Color(#colorLiteral(red: 0, green: 0.8134518862, blue: 0.9959517121, alpha: 1)))),
+        ])
+        return data
+    }
+    
+    // 차트의 데이터 특징 설정하고 만들기
+    func generateLineChartDataSet(dataSetEntries: [ChartDataEntry], color: UIColor, fillColor: UIColor) -> LineChartDataSet{
+        let dataSet = LineChartDataSet(entries: dataSetEntries, label: "")
+        dataSet.colors = [color]
+        dataSet.mode = .cubicBezier
+        dataSet.circleRadius = 5
+        dataSet.circleHoleColor = UIColor(Color(#colorLiteral(red: 0.003921568627, green: 0.231372549, blue: 0.431372549, alpha: 1)))
+        dataSet.fill = Fill.fillWithColor(fillColor)
+        dataSet.drawFilledEnabled = true
+        dataSet.setCircleColor(UIColor.clear)
+        dataSet.lineWidth = 2
+        dataSet.valueTextColor = color
+        dataSet.valueFont = UIFont(name: "Avenir", size: 12)!
+        return dataSet
+    }
+    
+    // 차트의 x값을 string값으로 변환
+    class CustomChartFormatter: NSObject, IAxisValueFormatter {
+        var times: [String]
+        
+        init(times: [String]) {
+            self.times = times
+        }
+        
+        public func stringForValue(_ value: Double, axis: AxisBase?) -> String {
+            print(value)
+            return times[Int(value-1)]
+        }
+    }
+}
--- PaceKeeper/ResultView.swift
+++ PaceKeeper/ResultView.swift
@@ -7,6 +7,7 @@
 
 import SwiftUI
 import MapKit
+import Charts
 
 struct ResultView: View {
     private let timerProcess: TimerProcess
@@ -16,26 +17,29 @@ struct ResultView: View {
     }
     
     var body: some View {
-        VStack{
-            // 수치 정보 보여주기
-            Group{
-                makeTotalInfoView("제한 속도", String(format: "%.1f", timerProcess.data.selectedSpeed) + " km/h")
-                Divider().padding(.horizontal)
-                makeTotalInfoView("평균 속도", String(format: "%.1f", 111) + " km/h")
-                Divider().padding(.horizontal)
-                makeTotalInfoView("총 이동시간", String(timerProcess.data.processedTime) + " s")
-                Divider().padding(.horizontal)
-                makeTotalInfoView("총 이동거리", String(format: "%.2f", timerProcess.data.movedDistance) + " km")
-                Divider().padding(.horizontal)
-                makeTotalInfoView("총 칼로리", String(format: "%.1f", timerProcess.data.consumedCalorie) + " kcal")
-                Divider().padding(.horizontal)
+        ScrollView{
+            VStack{
+                // 수치 정보 보여주기
+                Group{
+                    makeTotalInfoView("제한 속도", String(format: "%.1f", timerProcess.data.selectedSpeed) + " km/h")
+                    Divider()
+                    makeTotalInfoView("평균 속도", String(format: "%.1f", 111) + " km/h")
+                    Divider()
+                    makeTotalInfoView("총 이동시간", String(timerProcess.data.processedTime) + " s")
+                    Divider()
+                    makeTotalInfoView("총 이동거리", String(format: "%.2f", timerProcess.data.movedDistance) + " km")
+                    Divider()
+                    makeTotalInfoView("총 칼로리", String(format: "%.1f", timerProcess.data.consumedCalorie) + " kcal")
+                    Divider()
+                }
+                // 시간에 따른 속도 그래프 보여주기
+                makeChartView()
+                // 지도에 이동경로 보여주기
+                makeMapView()
+                Spacer()
             }
-            // 시간에 따른 속도 그래프 보여주기
-            
-            // 지도에 이동경로 보여주기
-            makeMapView()
-            Spacer()
-        }
+        }.padding(.horizontal)
+        
     }
     
     // 수치로 나타낼 수 있는 정보에 대한 뷰 만들기
@@ -48,7 +52,7 @@ struct ResultView: View {
             Text(content)
                 .foregroundColor(Color(hex: "0277B6"))
                 .font(.system(size: 20, weight: Font.Weight.bold))
-        }.padding(.horizontal)
+        }
     }
     
     // MapView 띄우기
@@ -59,7 +63,25 @@ struct ResultView: View {
         let mapSpan = MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01)
         // 보여줄 화면
         let mapRegion = MKCoordinateRegion(center: mapCenter, span: mapSpan)
-        return MapView(region: mapRegion, lineCoordinates: timerProcess.data.coordinates)
+        return MapView(region: mapRegion, lineCoordinates: timerProcess.data.coordinates).frame(height: 200)
+    }
+    
+    // ChartView 띄우기
+    func makeChartView() -> some View{
+        // 차트의 x값
+        let xVals = ["a", "b", "c", "d", "e", "f", "g"]
+        // 차트의 y값
+        let yVals = [
+            ChartDataEntry(x: 1, y: 1),
+            ChartDataEntry(x: 2, y: 3),
+            ChartDataEntry(x: 3, y: 2),
+            ChartDataEntry(x: 4, y: 7),
+            ChartDataEntry(x: 5, y: 5),
+            ChartDataEntry(x: 6, y: 4),
+            ChartDataEntry(x: 7, y: 6)
+        ]
+        // 보여줄 화면
+        return ChartView(xVals: xVals, yVals: yVals).frame(height: 200)
     }
 }
 
